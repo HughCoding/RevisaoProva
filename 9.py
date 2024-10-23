5efb74ba1d41b2731032fb78677a4a724bc1eebf
HughCoding <160680383+HughCoding@users.noreply.github.com>
2024-10-23 16:32:02 -0300

    Atualizar o 9.py

--- 9.py
+++ 9.py
@@ -8,7 +8,9 @@ else:
     else:
         print(x)
 
-
+# resposta esperada =
 # Para alpha > 0.1, ele retorna uma mensagem de erro.
 # Para alpha <= 0.1 e x < 0, ele retorna o produto de alpha e x.
 # Para alpha <= 0.1 e x >= 0, ele retorna o valor de x.
+
+# Nao escrevi o que ele retorna apenas os valores de menor ou igual a e maior ou igual a
